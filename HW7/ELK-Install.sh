0a4f3ef0aaf69cd2de1fc55121281504dacfe36e
kivanov87 <111631139+kivanov87@users.noreply.github.com>
2023-03-22 16:44:11 +0200

    kib yml

--- HW7/ELK-Install.sh
+++ HW7/ELK-Install.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-echo "* Install elasticsearch"
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.6.2-x86_64.rpm
-sudo rpm -Uvh elasticsearch-*.rpm
-
-echo "* Copying Elasticsearch Config Files"
-sudo cp /vagrant/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-sudo cp /vagrant/jvm.elastic /etc/elasticsearch/jvm.options.d/jvm.options
-
-echo "* reboot services"
-sudo systemctl daemon-reload
-sudo systemctl enable elasticsearch
-sudo systemctl start elasticsearch
-
 echo "* Install Logstash"
 wget https://artifacts.elastic.co/downloads/logstash/logstash-8.6.2-x86_64.rpm
 sudo rpm -Uvh logstash-*.rpm
+sudo cp /vagrant/jvm.logstash /etc/logstash/jvm.options
+
 
 echo "* reboot services"
 sudo systemctl daemon-reload
 sudo systemctl enable logstash
 sudo systemctl start logstash
 
-echo "* Install Logstash"
+echo "* Install kibana"
 wget https://artifacts.elastic.co/downloads/kibana/kibana-8.6.2-x86_64.rpm
 sudo rpm -Uvh kibana-*.rpm
 
+sudo cp /vagrant/kibana.yml /etc/logstash/kibana.yml
 
-mkdir /vagrant/logs
+echo "* Install logstash"
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.6.2-x86_64.rpm
+sudo rpm -Uvh elasticsearch-*.rpm
 
-vagrant scp vagrant.log default:/vagrant/logs/
+echo "* Copying Elasticsearch Config Files"
+sudo sudo rm /etc/elasticsearch/elasticsearch.yml -f
+sudo cp /vagrant/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp /vagrant/jvm.elastic /etc/elasticsearch/jvm.options.d/jvm.options
 
-egrep 'The\ generated\ password.*' vagrant.log | awk '{print $NF}'
+
+echo "* reboot services"
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch
+sudo systemctl start elasticsearch
