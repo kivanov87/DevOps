d8439701c79796f285a48f8320d395ab29fbd00c
kivanov87 <111631139+kivanov87@users.noreply.github.com>
2023-04-06 16:31:22 +0300

    hwork

--- HW8/p2/main.tf
+++ HW8/p2/main.tf
@@ -10,22 +10,32 @@ provider "docker" {
     host = "tcp://192.168.99.100:2375/" 
 }
 
-resource "docker_network" "app_network" {
-  name = "app_network"
+resource "docker_image" "img-web" {
+name = "shekeriev/bgapp-web:latest"
 }
 
-resource "docker_container" "db_container" {
-  name  = "bgapp-db"
-  image = "shekeriev/bgapp-db"
-  ports {
-    internal = 3306
-    external = 3306
+resource "docker_image" "img-db" {
+name = "shekeriev/bgapp-db:latest"
+}
+
+resource "docker_network" "app_network" {
+name = "app_network"
+}
+
+resource "docker_container" "con-web" {
+name = "site"
+image = docker_image.img-web.image_id
+ports {
+internal = "80"
+external = "80"
   }
-  env = [
-    "MYSQL_ROOT_PASSWORD=<password>",
-    "MYSQL_DATABASE=<database>"
-  ]
-  networks_advanced {
-    name = docker_network.app_network.name
+}
+
+resource "docker_container" "con-db" {
+name = "site"
+image = docker_image.img-web.image_id
+ports {
+internal = "8080"
+external = "8080"
   }
 }
