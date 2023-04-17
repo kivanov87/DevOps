671e409d18ab49d10b3f54eca91975d031f5ffee
kivanov87 <kalin.at.ivanov@gmail.com>
2023-04-18 01:15:11 +0300

    hw2 part 1 and part2

--- Infrastructure & Configuration Management/temp/HW2.1/image/output.tf
+++
@@ -1,7 +0,0 @@
-output "image_out" { 
-  value = "shekeriev/bgapp-web"
-}
-
-output "image_out_db" { 
-  value = "shekeriev/bgapp-db"
-}
--- Infrastructure & Configuration Management/temp/HW2.1/p2/2-finish/image/output.tf
+++
@@ -1,7 +0,0 @@
-output "image_out" { 
-  value = docker_image.img-web.name
-}
-
-output "image_out_db" { 
-  value = docker_image.img-db.name
-}
