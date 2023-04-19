a3ebe75ef136cdaff9043592610803f41ac16f97
kivanov87 <kalin.at.ivanov@gmail.com>
2023-04-19 23:51:10 +0300

    final

--- Infrastructure & Configuration Management/HW2/HW2.2/Install.sh
+++ Infrastructure & Configuration Management/HW2/HW2.2/Install.sh
@@ -5,9 +5,6 @@ dnf install ansible-core -y
 echo *INSTALL ANSIBLE-GALAXY *
 ansible-galaxy collection install -p /usr/share/ansible/collections ansible.posix
 
-echo *Create directory*
-mkdirexit
-
 echo *COPY INVENTORY AND CFG FILES*
 cp /vagrant/files/inventory .
 cp /vagrant/files/ansible.cfg .
